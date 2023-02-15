aea665820626d7ce55880af40459d85978f8cbd2
CipselaCorp <124727402+CipselaCorp@users.noreply.github.com>
2023-02-14 22:24:26 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,130 @@
+const APDS9960_I2C_ADDR = 0x39
+const APDS9960_ENABLE = 0x80
+const APDS9960_ATIME = 0x81
+const APDS9960_WTIME = 0x83
+const APDS9960_AILTIL = 0x84
+const APDS9960_AILTH = 0x85
+const APDS9960_AIHTL = 0x86
+const APDS9960_AIHTH = 0x87
+const APDS9960_PILT = 0x89
+const APDS9960_PIHT = 0x8B
+const APDS9960_PERS = 0x8C
+const APDS9960_CONFIG1 = 0x8D
+const APDS9960_PPULSE = 0x8E
+const APDS9960_CONTROL = 0x8F
+const APDS9960_CONFIG2 = 0x90
+const APDS9960_ID = 0x92
+const APDS9960_STATUS = 0x93
+const APDS9960_CDATAL = 0x94
+const APDS9960_CDATAH = 0x95
+const APDS9960_RDATAL = 0x96
+const APDS9960_RDATAH = 0x97
+const APDS9960_GDATAL = 0x98
+const APDS9960_GDATAH = 0x99
+const APDS9960_BDATAL = 0x9A
+const APDS9960_BDATAH = 0x9B
+let buf = pins.createBuffer(2);
+
+
+function setreg_lux(reg: number, dat: number): void {
+    buf[0] = reg;
+    buf[1] = dat;
+    pins.i2cWriteBuffer(APDS9960_I2C_ADDR, buf);
+    basic.pause(10)
+}
+
+function getreg_LUX(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_I2C_ADDR, reg, NumberFormat.UInt8BE);
+    basic.pause(10)
+    return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt8BE);
+}
+
+function getreg8L_LUX(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_I2C_ADDR, reg, NumberFormat.UInt8BE);
+    basic.pause(10)
+    return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt8LE);
+}
+
+function getUInt16BE_LUX(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_I2C_ADDR, reg, NumberFormat.UInt8BE);
+    basic.pause(10)
+    return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt16BE);
+}
+
+function get16BE_LUX(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_I2C_ADDR, reg, NumberFormat.UInt8BE);
+    basic.pause(10)
+    return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt16LE);
+}
+
+
+function PowerOn() {
+    let t =getreg_LUX(APDS9960_ENABLE)
+    t |= 0b00001011
+    setreg_lux(APDS9960_ENABLE, t)
+    basic.pause(10)
+}
+
+function Control() {
+    let t = getreg_LUX(APDS9960_CONTROL)
+    t |= 0b00000011
+    setreg_lux(APDS9960_CONTROL, t)
+    basic.pause(10)
+}
+
+
+function init() {
+
+    setreg_lux(APDS9960_ENABLE, 0)
+    basic.pause(10)
+    setreg_lux(APDS9960_ATIME, 0xFF)
+    basic.pause(10)
+    setreg_lux(APDS9960_WTIME, 0xFF)
+    basic.pause(10)
+    setreg_lux(APDS9960_CONFIG1, 0x60)
+    basic.pause(10)
+    setreg_lux(APDS9960_PERS, 0x11)
+    basic.pause(10)
+    setreg_lux(APDS9960_AIHTH, 0)
+    basic.pause(10)
+    setreg_lux(APDS9960_AIHTL, 0xFFFF)
+    basic.pause(10)
+    setreg_lux(APDS9960_AILTIL, 0xFFFF)
+    basic.pause(10)
+    setreg_lux(APDS9960_AILTH, 0)
+    basic.pause(10)
+    Control()
+    PowerOn()
+  
+}
+//% color=#4c6ef5 weight=25 icon="\uf043" block="APDS9960"
+namespace CIP_APDS9960 {
+init();
+let illuminance = 0
+
+    /**
+         * Returns a number describing the lux 
+        */
+    //% blockId="APDS9960_read_LUX"
+    //% block="leer lux"
+    export function leer_lux(): number {
+        let b = getUInt16BE_LUX(APDS9960_STATUS)
+        let TL = getUInt16BE_LUX(APDS9960_AILTIL);
+        let TH = getUInt16BE_LUX(APDS9960_AIHTH);
+        let LH = getUInt16BE_LUX(APDS9960_AILTH);
+        let HL = getUInt16BE_LUX(APDS9960_AIHTL);
+  
+        let c = getUInt16BE_LUX(APDS9960_CDATAL);
+        basic.pause(10)
+        if ((c >= TH + LH) || (c <= TL + HL)) {
+            let r = getUInt16BE_LUX(APDS9960_RDATAH);
+            let g = getUInt16BE_LUX(APDS9960_GDATAH);
+            let b = getUInt16BE_LUX(APDS9960_BDATAH);
+            illuminance = (0.6 * r )+( 0.4 * g) - (0.5 * b);
+            if (illuminance < 0) illuminance = Math.abs(illuminance)
+        }
+        basic.pause(500)
+        //basic.showNumber(illuminance)
+        return illuminance
+    }
+}
