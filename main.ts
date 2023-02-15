339854d7fc9984f13d8ef12fa5550a6d6bb9c0c2
CipselaCorp <124727402+CipselaCorp@users.noreply.github.com>
2023-02-14 22:33:28 -0500

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,7 @@ function get16BE_LUX (reg: number) {
     basic.pause(10)
     return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt16LE, false)
 }
-function getreg8L_LUX (reg: number) {
-    pins.i2cWriteNumber(
-    APDS9960_I2C_ADDR,
-    reg,
-    NumberFormat.UInt8BE,
-    false
-    )
-    basic.pause(10)
-    return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt8LE, false)
-}
+
 function getUInt16BE_LUX (reg: number) {
     pins.i2cWriteNumber(
     APDS9960_I2C_ADDR,
@@ -35,7 +26,7 @@ function setreg_lux (reg: number, dat: number) {
 basic.pause(10)
 }
 function Control () {
-    u = getreg_LUX(APDS9960_CONTROL)
+    let u = getreg_LUX(APDS9960_CONTROL)
     u |= 0b00000011
 setreg_lux(APDS9960_CONTROL, u)
     basic.pause(10)
@@ -73,50 +64,38 @@ function getreg_LUX (reg: number) {
     return pins.i2cReadNumber(APDS9960_I2C_ADDR, NumberFormat.UInt8BE, false)
 }
 function PowerOn () {
-    t = getreg_LUX(APDS9960_ENABLE)
+    let t = getreg_LUX(APDS9960_ENABLE)
     t |= 0b00001011
-setreg_lux(APDS9960_ENABLE, t)
+    setreg_lux(APDS9960_ENABLE, t)
     basic.pause(10)
 }
-let t = 0
-let u = 0
-let APDS9960_CONTROL = 0
-let APDS9960_CONFIG1 = 0
-let APDS9960_PERS = 0
-let APDS9960_AIHTH = 0
-let APDS9960_AIHTL = 0
-let APDS9960_AILTH = 0
-let APDS9960_AILTIL = 0
-let APDS9960_WTIME = 0
-let APDS9960_ATIME = 0
-let APDS9960_ENABLE = 0
-let APDS9960_I2C_ADDR = 0
-APDS9960_I2C_ADDR = 57
-APDS9960_ENABLE = 128
-APDS9960_ATIME = 129
-APDS9960_WTIME = 131
-APDS9960_AILTIL = 132
-APDS9960_AILTH = 133
-APDS9960_AIHTL = 134
-APDS9960_AIHTH = 135
-let APDS9960_PILT = 137
-let APDS9960_PIHT = 139
-APDS9960_PERS = 140
-APDS9960_CONFIG1 = 141
-let APDS9960_PPULSE = 142
-APDS9960_CONTROL = 143
-let APDS9960_CONFIG2 = 144
-let APDS9960_ID = 146
-let APDS9960_STATUS = 147
-let APDS9960_CDATAL = 148
-let APDS9960_CDATAH = 149
-let APDS9960_RDATAL = 150
-let APDS9960_RDATAH = 151
-let APDS9960_GDATAL = 152
-let APDS9960_GDATAH = 153
-let APDS9960_BDATAL = 154
-let APDS9960_BDATAH = 155
+
+const APDS9960_I2C_ADDR = 57
+const APDS9960_ENABLE = 128
+const APDS9960_ATIME = 129
+const APDS9960_WTIME = 131
+const APDS9960_AILTIL = 132
+const APDS9960_AILTH = 133
+const APDS9960_AIHTL = 134
+const APDS9960_AIHTH = 135
+
+const APDS9960_PERS = 140
+const APDS9960_CONFIG1 = 141
+const APDS9960_PPULSE = 142
+const APDS9960_CONTROL = 143
+const APDS9960_CONFIG2 = 144
+const APDS9960_ID = 146
+const APDS9960_STATUS = 147
+const APDS9960_CDATAL = 148
+const APDS9960_CDATAH = 149
+const APDS9960_RDATAL = 150
+const APDS9960_RDATAH = 151
+const APDS9960_GDATAL = 152
+const APDS9960_GDATAH = 153
+const APDS9960_BDATAL = 154
+const APDS9960_BDATAH = 155
 let buf = pins.createBuffer(2);
+
 namespace CIP_APDS9960 {
 init();
 let illuminance = 0
